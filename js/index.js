9eab24259d8e743ede24efc0ca8f0f02f7281991
Matus Backor <MatusB@kentico.com>
2019-02-08 15:44:11 +0100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -13,3 +13,11 @@ console.log("Hello Kentico Cloud, from JavaScript");
 // DocSection: multiple_assalamualaikum_alealamia2
 console.log("Hello Kentico Cloud, from JavaScript");
 // EndDocSection
+
+// DocSection: multiple_multiple
+console.log("Hello Kentico Cloud, from JavaScript");
+// EndDocSection
+
+// DocSection: single_single
+console.log("Hello Kentico Cloud, from JavaScript");
+// EndDocSection
