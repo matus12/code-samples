3b99fdd5ef6bf47ae33b6e91708beb9a7599c9b4
Matus Backor <MatusB@kentico.com>
2019-02-08 14:58:52 +0100

    Update javicka.java

--- java/javicka.java
+++ java/javicka.java
@@ -1,3 +1,7 @@
 // DocSection: multiple_hello_world
 Console.WriteLine("Hello Kentico Cloud, from Javicka");
 // EndDocSection
+
+// DocSection: multiple_test_java
+Console.WriteLine("Hello Kentico Cloud, from Javicka");
+// EndDocSection
