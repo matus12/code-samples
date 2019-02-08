366bff63abcae45caa0332846178ae90e48bfff2
Matus Backor <MatusB@kentico.com>
2019-02-08 13:19:51 +0100

    Create file.js

---
+++ js/file.js
@@ -0,0 +1,7 @@
+// DocSection: single_it1
+console.log("Hello Kentico Cloud, from JavaScript neeeeeeeeeeeeeeeeew");
+// EndDocSection
+
+// DocSection: single_it2
+console.log("Hello Kentico Cloud, from JavaScript neeeeeeeeeeeeeeeeew2");
+// EndDocSection
