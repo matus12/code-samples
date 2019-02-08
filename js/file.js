84a5abee32b88fb586daccb5f639d550f81fd3cc
Matus Backor <MatusB@kentico.com>
2019-02-08 13:22:05 +0100

    Delete file.js

--- js/file.js
+++
@@ -1,7 +0,0 @@
-// DocSection: single_it1
-console.log("Hello Kentico Cloud, from JavaScript neeeeeeeeeeeeeeeeew");
-// EndDocSection
-
-// DocSection: single_it2
-console.log("Hello Kentico Cloud, from JavaScript neeeeeeeeeeeeeeeeew2");
-// EndDocSection
