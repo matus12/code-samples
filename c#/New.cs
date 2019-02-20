77e2f69c08491a774750b521065341d7040841fe
Matus Backor <MatusB@kentico.com>
2019-02-20 08:26:13 +0100

    Create New.cs

---
+++ c#/New.cs
@@ -0,0 +1,7 @@
+// DocSection: multiple_hello_world
+Console.WriteLine("Hello Kentico Cloud, from C#");
+// EndDocSection
+
+// DocSection: single_hello_world2
+Console.WriteLine("Hi C#");
+// EndDocSection
