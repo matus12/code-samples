92ef927ce968f6c9472c4a21ea374a3c4cb13cf5
Matus Backor <MatusB@kentico.com>
2019-04-10 14:43:36 +0200

    Update Program.cs

--- net/Program.cs
+++ net/Program.cs
@@ -2,6 +2,3 @@
 Console.WriteLine("Hello Kentico Cloud, from C#");
 // EndDocSection
 
-// DocSection: what_the_hell_is_going_on
-Console.WriteLine("Nuffin bruh oh yeah buddyyyy sahdsahiudsahisud");
-// EndDocSection
